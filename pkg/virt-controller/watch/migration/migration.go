2ad06f9b27af8916f407bb49026e687cdf840fc1
dsionov <dsionov@redhat.com>
2025-01-12 15:50:50 +0200

    migration-controller: Attach machine type label to node selector
    When preparing the target pod for a migration, ensure that the
    NodeSelector includes a label corresponding to the VMI's machine type.
    This guarantees that the target node explicitly supports the required
    machine type, preventing migration failures due to incompatibilities.
    
    By adding the machine type label to the NodeSelector, the scheduler will
     only consider nodes that have the appropriate label with a value of
     either true or deprecated, ensuring compatibility.
    
    Signed-off-by: Daniel Sionov dsionov@redhat.com

--- pkg/virt-controller/watch/migration/migration.go
+++ pkg/virt-controller/watch/migration/migration.go
@@ -755,6 +755,10 @@ func (c *Controller) createTargetPod(migration *virtv1.VirtualMachineInstanceMig
 		if err != nil {
 			return err
 		}
+
+		if machineType := vmi.Spec.Domain.Machine.Type; machineType != "" {
+			prepareNodeSelectorForMachineType(machineType, templatePod)
+		}
 	}
 
 	matchLevelOnTarget := c.clusterConfig.GetMigrationConfiguration().MatchSELinuxLevelOnMigration
@@ -2092,6 +2096,11 @@ func prepareNodeSelectorForHostCpuModel(node *k8sv1.Node, pod *k8sv1.Pod, source
 	}
 
 	return nil
+}
+
+func prepareNodeSelectorForMachineType(machineType string, sourcePod *k8sv1.Pod) {
+	machineTypeLabelKey := virtv1.SupportedMachineTypeLabel + machineType
+	sourcePod.Spec.NodeSelector[machineTypeLabelKey] = ""
 }
 
 func isNodeSuitableForHostModelMigration(node *k8sv1.Node, requiredNodeLabels map[string]string) bool {
